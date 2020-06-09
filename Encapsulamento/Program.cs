bd2a238d00f1d68c95023cf173971871301e1ebb
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-09 14:28:38 -0300

    Encapsulamento manual

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstruturesAula51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            //Exemplo negativo
+
+            //p.Quantidade = - 10; // Sem encapsular o programador pode fazer isso aqui. Erro!
+
+            /*Um programa seguro, so permite q isso aconteca atraves de uma operacao de adicionar 
+               e remover produto   
+            */
+
+
+            // Alterar o nome atraves do método SetNome:
+
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine();
+
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine();
+
+            Console.WriteLine(p.GetQuantidade());
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
